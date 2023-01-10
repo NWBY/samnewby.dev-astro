7e6c85bb39242bf099905e586c71ba257017c488
Sam Newby <sam.newby@tails.com>
2023-01-10 20:18:42 +0000

    feat: initial commit

---
+++ src/site.config.ts
@@ -0,0 +1,10 @@
+const siteMeta = {
+  title: "Sam Newby - Software Engineer",
+  description:
+    "Full-Stack Software Engineer at Tails.com working with Python, TypeScript, and Vue.js. Building Serverless projects with AWS SAM in my spare time.",
+  lang: "en-GB",
+  ogLocale: "en_GB",
+  githubUrl: "https://github.com/NWBY",
+};
+
+export default siteMeta;
