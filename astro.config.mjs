f2a001638c1fa24f8ef7d0c8e9b618169be34543
Sam Newby <sam.newby@tails.com>
2023-01-11 18:09:07 +0000

    chore: huge progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -9,7 +9,13 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
 
+import tokyoNight from "./tokyo-night.json" assert { type: "json" };
+import tokyoNightStorm from "./tokyo-night-storm.json" assert { type: "json" };
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), alpinejs()]
+  integrations: [tailwind(), mdx(), alpinejs()],
+  markdown: {
+    shikiConfig: { theme: tokyoNightStorm },
+  },
 });
